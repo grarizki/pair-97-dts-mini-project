a4d548eafcc3bf8e679a31e32ca59a6995ad9844
grarizki <28901232+grarizki@users.noreply.github.com>
2022-07-23 15:37:43 +0700

    rm css and add several rows

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import './App.css';
 import React from 'react';
 import Row from './components/Row/Row';
 import requests from './requests.js'
+import Banner from './components/Banner/Banner';
 
 function App() {
 
 
   return (
-    <div className="App">
+    <div className="App bg-gray-900 m-0">
+      <Banner fetchUrl={requests.fetchNetflixOriginals} />
       <Row title="Movies" fetchUrl={requests.fetchNetflixOriginals} />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
