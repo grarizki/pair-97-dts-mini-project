843bde68639247164c98493481434acd5446e6f1
grarizki <28901232+grarizki@users.noreply.github.com>
2022-07-23 15:38:21 +0700

    add row component

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -22,11 +22,11 @@ function Row({ title, fetchUrl }) {
 
     return (
         <div className="row">
-            <h2>{title}</h2>
+            <h2 className='text-white'>{title}</h2>
 
             <div className="row_posters">
                 {movies.map(movie => (
-                    <img className="row_poster" src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                    <img key={movie.id} className="row_poster" src={`${base_url}${movie.poster_path}`} alt={movie.name} />
                 ))}
 
             </div>
